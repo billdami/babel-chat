7a9a154e502e22fd857db2d8dc6fc814d820e3d0
Bill Dami <billdami@gmail.com>
2021-05-16 14:54:48 -0400

    cleanup boilerplate, add typescript, tailwind css

---
+++ src/App/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders paragrah', () => {
+  render(<App />);
+  const pElement = screen.getByText(/welcome to babel chat!/i);
+  expect(pElement).toBeInTheDocument();
+});
---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+interface AppProps {
+
+}
+
+const App: FC<AppProps> = () => {
+  return (
+    <div className="App">
+      <p>
+          welcome to babel chat!
+        </p>
+    </div>
+  );
+}
+
+export default App;
