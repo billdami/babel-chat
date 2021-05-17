a00ed708a86afbfac41fd8a9e7f231c5b8660c7a
Bill Dami <billdami@gmail.com>
2021-05-16 20:03:46 -0400

    integrate and test firebase

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,41 @@
 import React, { FC } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+import { useListVals } from 'react-firebase-hooks/database';
 
-interface AppProps {
+const db = firebase.database();
+const usersRef = db.ref('users');
 
+// TODO reorganize types
+interface User {
+  id: string;
+  ref: string;
+  username: string;
+  email: string;
 }
 
+interface AppProps {}
+
 const App: FC<AppProps> = () => {
+  const [users, isLoading, /*error*/] = useListVals<User>(usersRef, { keyField: 'id', refField: 'ref' });
+
   return (
     <div className="App">
       <p>
-          welcome to babel chat!
-        </p>
+        welcome to babel chat!
+      </p>
+      <ul>
+        {users?.map(user => (
+          <li key={user.id}>
+            <div>{user.username}</div>
+            <div>{user.email}</div>
+          </li>
+        ))}
+        {isLoading && (
+          <div>Loading&hellip;</div>
+        )}
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import reportWebVitals from './reportWebVitals';
+import './setupFirebase';
 import './index.css';
 import App from './App/App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
