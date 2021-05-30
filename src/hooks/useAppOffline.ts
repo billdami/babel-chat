503adf656d486e488a67c848661067ba24b429fc
Bill Dami <billdami@gmail.com>
2021-05-30 06:53:57 -0400

    make hook exports more consistent

--- src/hooks/useAppOffline.ts
+++ src/hooks/useAppOffline.ts
@@ -5,10 +5,12 @@ import firebase from 'firebase/app';
 import { useObject } from 'react-firebase-hooks/database';
 import { useMemo } from 'react';
 
-export const useIsAppOffline = () => {
+const useIsAppOffline = () => {
   const db = firebase.database();
   const ref = db.ref('APP_OFFLINE');
   const [snapshot, isAppOfflineLoading] = useObject(ref);
   const isAppOffline = useMemo<boolean>(() => snapshot?.val() === true, [snapshot]);
   return { isAppOffline, isAppOfflineLoading };
 };
+
+export default useIsAppOffline;
