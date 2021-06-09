4fbe17495a45bb7cc27377d8b11511c35dd83762
Bill Dami <billdami@gmail.com>
2021-06-09 09:50:55 -0400

    improve message list scrolling behavior

--- src/routes/Main/Chat/components/MessageList/index.tsx
+++ src/routes/Main/Chat/components/MessageList/index.tsx
@@ -65,11 +65,6 @@ const MessageList: FC<MessageListProps> = ({
   const lastMessages = useRef<ChatMessageRecord[] | null | undefined>(messages);
   const containerElement = useRef<HTMLDivElement>(null);
 
-  // TODO automatic load more on scroll up
-  // const loadMoreElement = useRef<HTMLDivElement>(null);
-  // const isLoadMoreVisible = useIsVisible(loadMoreElement.current);
-  // const [debouncedIsLoadMoreVisible] = useDebounce(isLoadMoreVisible, 250);
-
   const isSelf = !!originUser && !!destUser && originUser.id === destUser.id;
   const emptyChat = !isSelf && !isBlocked && !isLoading && !messages?.length;
   const mayHaveMore = !!messages?.length && messages?.length >= limit;
@@ -96,30 +91,28 @@ const MessageList: FC<MessageListProps> = ({
     return map;
   }, [originUser, originChat, destUser, destUserId]);
 
+  const loadMore = useCallback(() => {
+    if (canLoadMore && !isLoading) {
+      // TODO store a temp copy of the current messages before loading more
+      // which will be rendering while loading the next page, to avoid
+      // the blank white page flicker that happens when the `messages` is empty
+      // once the query/limit is updated
+      setLimit(limit + MSG_PAGE_LIMIT);
+      setIsLoadingMore(true);
+    }
+  }, [canLoadMore, isLoading, limit]);
+
   const onContainerScroll = useDebouncedCallback((event: ReactUIEvent<HTMLDivElement, UIEvent>) => {
     const el = containerElement.current;
     if (el) {
       const maxScroll = el.scrollHeight - el.clientHeight;
       setIsScrolledUp(el.scrollTop + MSG_SCROLLED_UP_THRESHOLD < maxScroll);
-      // TODO (possibly) if scrolled to the very top (scrollTop <= MSG_LOAD_MORE_BTN_HEIGHT)
-      // and more messages can be loaded, trigger loading the previous page
-      // this would replace ths useIsVisible() logic for handling this
+      // when scrolled all the way to the top, auto load older messages, if there are any
+      if (el.scrollTop === 0 && canLoadMore && !isLoading && !isLoadingMore) {
+        loadMore();
+      }
     }
-  }, 250);
-
-  const loadMore = useCallback(() => {
-    if (canLoadMore) {
-      setLimit(limit + MSG_PAGE_LIMIT);
-      setIsLoadingMore(true);
-    }
-  }, [canLoadMore, limit]);
-
-  // TODO automatic load more on scroll up
-  // useEffect(() => {
-  //   if (!isLoading && !isLoadingMore && debouncedIsLoadMoreVisible) {
-  //     loadMore();
-  //   }
-  // }, [isLoading, isLoadingMore, debouncedIsLoadMoreVisible, loadMore]);
+  }, 300);
 
   useEffect(() => {
     // if new messages were added, update the user's dateLastSeen to mark them as "read"
@@ -145,24 +138,21 @@ const MessageList: FC<MessageListProps> = ({
   useLayoutEffect(() => {
     const el = containerElement.current;
     if (el) {
-      // scroll to the bottom when:
-      //  - loading the initial list
-      //  - new messages are added
-      //  - the container is NOT scrolled up
-      if (
-        messages !== prevMessages &&
-        messages?.length &&
-        (!isScrolledUp || isFirstMount.current)
-      ) {
-        const msgsDiff = messages.length - (lastMessages.current?.length ?? 0);
-
-        // dont scroll down when loading older messages
-        if (!isLoadingMore) {
+      // scroll when loading the initial list or new messages are added
+      if (messages !== prevMessages && messages?.length) {
+        if (!isLoadingMore && (!isScrolledUp || isFirstMount.current)) {
+          // scroll to the bottom on initial mount and when not loading older messages
+          // and not when the container is scrolled up
           el.scrollTo({
             top: el.scrollHeight - el.clientHeight,
-            behavior: isFirstMount.current ? 'auto' : 'smooth',
+            // TODO try to allow 'smooth' behavior when isFirstMount is false again
+            // there is a race condition bug of some kind that makes the scroll to
+            // bottom happen too late when smooth scrolling when recieving new messages
+            behavior: 'auto',
           });
-        } else {
+        } else if (isLoadingMore) {
+          // maintain the current scroll position when older messages are loaded
+          const msgsDiff = messages.length - (lastMessages.current?.length ?? 0);
           // get the first message before the new messages that were prepended
           const headMsg = msgsDiff > 0 && messages[msgsDiff];
 
@@ -172,8 +162,7 @@ const MessageList: FC<MessageListProps> = ({
               `[data-msg-id="${headMsg.id}"]`
             );
             if (msgEl) {
-              containerElement.current.scrollTop =
-                msgEl.offsetTop - containerElement.current.offsetTop - MSG_LOAD_MORE_BTN_HEIGHT;
+              el.scrollTop = msgEl.offsetTop - el.offsetTop - MSG_LOAD_MORE_BTN_HEIGHT;
             }
           }
           // when older messages are done loading, reset the flag
@@ -225,7 +214,6 @@ const MessageList: FC<MessageListProps> = ({
           </div>
         )}
 
-        {/* TODO when "load more" button is visible, debounce a automatic loading of more messages */}
         {canLoadMore && (
           <div className="px-2 md:px-3 py-3">
             <Button
