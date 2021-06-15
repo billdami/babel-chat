5f43ca2f9bc04ec4f95ee97231c01ec4b7b7257f
Bill Dami <billdami@gmail.com>
2021-06-15 16:55:24 -0400

    fix facebook share link

--- src/components/FacebookShareButton/index.tsx
+++ src/components/FacebookShareButton/index.tsx
@@ -13,14 +13,14 @@ const FacebookShareButton: FC<FacebookShareButtonProps> = () => {
     <div ref={container}>
       <div
         className="fb-share-button"
-        data-href="https://developers.facebook.com/docs/plugins/"
+        data-href="https://babelchat.online/"
         data-layout="button_count"
         data-size="large"
       >
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fbabelchat.online%2F&amp;src=sdkpreparse"
           className="fb-xfbml-parse-ignore"
         >
           Share
