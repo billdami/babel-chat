e6fca1fc9427d8e7d214852790c0c148ad6a2dd0
Bill Dami <billdami@gmail.com>
2021-06-23 13:20:04 -0400

    add donate link to feedback dialog

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,5 @@
+export const MOBILE_MAX_WIDTH = 768;
 export const COPYRIGHT_YEAR = 2021;
 export const COPYRIGHT_LINE = `© ${COPYRIGHT_YEAR} babel chat. all rights reserved.`;
 export const DEFAULT_TITLE = 'babel chat - Meet and chat with people from around the world.';
-export const MOBILE_MAX_WIDTH = 768;
+export const DONATE_URL = 'https://www.buymeacoffee.com/babelchat';
