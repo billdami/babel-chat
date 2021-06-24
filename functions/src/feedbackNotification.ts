1cb5899ac471b862d0e819e2146e73df74db8331
Bill Dami <billdami@gmail.com>
2021-06-24 14:47:55 -0400

    Feedback email notification (#38)
    * first pass at cloud function to send email notifications for feedback
    
    * clean up feedback email message body
    
    * minor wording change

---
+++ functions/src/feedbackNotification.ts
@@ -0,0 +1,67 @@
+import * as functions from 'firebase-functions';
+import * as Mail from 'nodemailer/lib/mailer';
+import * as nodemailer from 'nodemailer';
+
+import { CloudFunction } from 'firebase-functions';
+import { DataSnapshot } from 'firebase-functions/lib/providers/database';
+
+import { DB_NAME_DEVELOPMENT, DB_NAME_PRODUCTION } from './utils/constants';
+import { setupApp } from './utils/firebase';
+
+const funcFeedbackNotification = (env: 'production' | 'development'): CloudFunction<DataSnapshot> =>
+  functions.database
+    // TODO make this better - store db URLs in firebase configs
+    .instance(env === 'production' ? DB_NAME_PRODUCTION : DB_NAME_DEVELOPMENT)
+    .ref('/feedback_messages/{feedback_message_id}')
+    .onCreate(async (snapshot: functions.database.DataSnapshot) => {
+      const app = setupApp(env);
+      const config = functions.config();
+      const deleteAppInstance = () => app.delete().catch(() => {});
+
+      try {
+        const feedbackMessage = snapshot.val();
+        const user = feedbackMessage.user;
+        const transporter: Mail = nodemailer.createTransport({
+          host: config.mail?.host,
+          port: Number(config.mail?.port),
+          secure: false,
+          auth: {
+            type: 'LOGIN',
+            user: config.mail?.user,
+            pass: config.mail?.password,
+          },
+        });
+
+        await transporter.sendMail({
+          to: config.mail?.to,
+          from: config.mail?.to,
+          subject: `[FEEDBACK] babel chat - ${user?.nickname}#${user?.uuid}`,
+          text: `New babelchat.online feedback message received:
+
+---------------
+Email: ${feedbackMessage.email}
+Nickname: ${user?.nickname}#${user?.uuid}
+Country: ${user?.country}
+Age: ${user?.age}
+Gender: ${user?.gender}
+---------------
+
+Message:
+
+${feedbackMessage.message}
+
+---------------
+Additional info:
+
+User ID: ${user?.id}
+Browser data:\n ${JSON.stringify(feedbackMessage.browser, undefined, 2)}`,
+        });
+        functions.logger.info('sent feedback email notification', feedbackMessage);
+      } catch (err) {
+        functions.logger.error('sending feedback email notification failed', err);
+      }
+
+      return deleteAppInstance();
+    });
+
+export default funcFeedbackNotification;
